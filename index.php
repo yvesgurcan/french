fd75813f9ff5f0428380a65267321d3ed7be8676
gerkhin <gurcan.yves@gmail.com>
2017-01-23 20:04:08 -0800

    first commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+    /*
+        This is a simple redirect. In the future, I intend to add other types of French exercises which will have their own folder. This page will then be a menu to the type of the exercise the user want to work on.
+    */
+    header('Location: Readme.MD');
+    exit();
+?>
